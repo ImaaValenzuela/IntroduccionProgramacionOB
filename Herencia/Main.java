5bc0726d74969e5e6a36122968c3dd73c22d28df
Imanol <imanolvalenzuela03@gmail.com>
2023-01-20 10:24:56 -0300

    Practica Clases Abstractas JAVA - Introduccion a la Programacion

--- Herencia/Main.java
+++ Herencia/Main.java
@@ -6,26 +6,44 @@ public class Main{
         coche.velocidadMaxima = 180;
         coche.matricula = "AB 123 CD";
         coche.puertas = 5;
+        coche.setSonido("FIUUUUM");
         Moto moto = new Moto();
         moto.velocidadMaxima = 240;
         moto.matricula = "EF 456 GH";
         System.out.println("Celta tiene: "+ coche.puertas + " puertas");
+        System.out.println(coche.getSonido());
         System.out.println(moto.velocidadMaxima);
     }
 }
 
-class Vehiculo{
+abstract class Vehiculo{
     int velocidadMaxima;
     String matricula;
+    String sonido;
+    abstract public String getSonido();
+    abstract public void setSonido(String sonido);
+
+
 }
 
 class Coche extends Vehiculo{
     int puertas;
+    public String getSonido(){
+        return "El auto hace:" + this.sonido;
+    }
+    public void setSonido(String sonido){
+        this.sonido = sonido;
+    }
 }
 
 final class Celta extends Coche{
 }
 
 class Moto extends Vehiculo{
-
+        public String getSonido(){
+        return "La moto hace:" + this.sonido;
+    }
+    public void setSonido(String sonido){
+        this.sonido = sonido;
+    }
 }
